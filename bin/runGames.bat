63c4cbe8c204599e39b9702ef620374e8af186a8
Alan Chusuei <alan.chusuei@gmail.com>
2014-08-14 16:06:24 -0400

    Create runGames.bat

---
+++ bin/runGames.bat
@@ -0,0 +1,6 @@
+java com.cards.TexasHoldEmGame Ac,Ad Kd,Qs Ah,Kh,Qd,Qc,3h
+java com.cards.TexasHoldEmGame 3c,Ac Ad,Qs Ah,Kh,Qd,Qc,3h
+java com.cards.TexasHoldEmGame 5d,7d Qd,As 9d,Kh,Qd,jc,Td
+java com.cards.TexasHoldEmGame Ad,Kc 5c,Qc 7c,9c,4c,Jc,3h
+java com.cards.TexasHoldEmGame Ac,Qd Ad,Ks Ah,9h,4d,2c,3h
+java com.cards.TexasHoldEmGame Ac,5d Kd,js 7h,4d,Qd,8c,3h
