c42d49ebfb97156bc41fa06a3836940342809f30
Alan Chusuei <achusuei@groupcommerce.com>
2014-09-02 14:29:05 -0400

    add thrid player for one scenario

--- bin/runGames.bat
+++ bin/runGames.bat
@@ -4,4 +4,5 @@ java com.cards.TexasHoldEmGame 5d,7d Qd,As 9d,Kh,Qd,jc,Td
 java com.cards.TexasHoldEmGame Ad,Kc 5c,Qc 7c,9c,4c,Jc,3h
 java com.cards.TexasHoldEmGame Ac,Qd Ad,Ks Ah,9h,4d,2c,3h
 java com.cards.TexasHoldEmGame Ac,5d Kd,js 7h,4d,Qd,8c,3h
-java com.cards.TexasHoldEmGame Kc,Ad 2d,2d Qh,4d,Qd,4c,3h
+
+java com.cards.TexasHoldEmGame Kc,Ad 2d,2d Qs,Qc Qh,4d,Qd,4c,3h
